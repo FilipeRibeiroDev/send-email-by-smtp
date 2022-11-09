127ba050cb1ffad6f348619e3a5ab620db72329f
Filipe Ribeiro <68390743+FilipeRibeiroDev@users.noreply.github.com>
2022-11-09 13:21:32 -0300

    First Commit

---
+++ SendEmail/Program.cs
@@ -0,0 +1,15 @@
+﻿using SendEmail;
+
+var outlook = new Email("smtp.office365.com", "", ENV.PASSWORD);
+
+outlook.SendEmail(
+emailsTo: new List<string>
+{
+    ""
+},
+subject: "Teste",
+body: "Segue Anexo",
+attachments: new List<string>
+{
+    @"C:\temp\excel\Dados.xlsx"
+});
